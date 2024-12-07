ac1d22e16dc5fd568d84e90ec54449f64787cac7
Toby Davis <pencilcaseman@gmail.com>
2024-12-07 20:51:29 +0000

    Slow solution for day 6

--- tests/year2024/day06_test.rs
+++ tests/year2024/day06_test.rs
@@ -40,6 +40,25 @@ const EXAMPLE_INPUT: &str = "\
 #.........
 ......#...";
 
+//   0123456789
+// 0 ....#.....
+// 1 .........#
+// 2 ..........
+// 3 ..#.......
+// 4 .......#..
+// 5 ..........
+// 6 .#.O^.....
+// 7 ........#.
+// 8 #.........
+// 9 ......#...
+//
+// (6, 3)
+// (7, 6)
+// (7, 7)
+// (8, 1)
+// (8, 3)
+// (9, 7)
+
 #[test]
 fn test_part1_minimal() {
     let input = parse(MINIMAL_INPUT);
@@ -55,10 +74,16 @@ fn test_part1() {
 #[test]
 fn test_contains_loop() {
     let input = parse(CONTAINS_LOOP);
-    assert!(trace_route(input.0.clone(), (3, 2), (-1, 0)));
+    assert!(matches!(
+        trace_route(input.0.clone(), (3, 2), (-1, 0)),
+        TraceResult::Loop(_)
+    ));
 
     let input = parse(MINIMAL_PART_2);
-    assert!(!trace_route(input.0.clone(), (3, 2), (-1, 0)));
+    assert!(matches!(
+        trace_route(input.0.clone(), (3, 2), (-1, 0)),
+        TraceResult::Exit(_)
+    ));
 }
 
 #[test]
