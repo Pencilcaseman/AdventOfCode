3452d62273566a6a83ee8134423dc920226766a9
Toby Davis <pencilcaseman@gmail.com>
2024-12-16 22:24:05 +0100

    hehe I'm so stupid

--- src/year2024/day09.rs
+++ src/year2024/day09.rs
@@ -19,7 +19,7 @@ pub fn parse(input: &str) -> Input {
 ///
 /// A derivation is given below:
 ///
-/// ```text
+/// ```math
 /// let x be the id
 /// let s be the starting index (start)
 /// let n be the end element (count - 1)
@@ -90,85 +90,140 @@ pub fn part1(input: &Input) -> usize {
 #[must_use]
 pub fn part2(input: &Input) -> usize {
     let mut heaps: [BinaryHeap<Reverse<usize>>; 10] = Default::default();
-    let mut sim_indices = vec![0; input.len()];
-    let mut sum = 0;
+    // let mut sum = 0;
 
-    let mut sim_index = 0;
-    let mut real_index = 0;
-    while real_index + 1 < input.len() {
-        // Compute the current sum and then subtract from it to avoid
-        // recalculating
-        sum += partial_checksum(real_index / 2, sim_index, input[real_index]);
+    // let mut sim_index = 0;
+    // let mut real_index = 0;
+    // while real_index + 1 < input.len() {
+    //     // Compute the current sum and then subtract from it to avoid
+    //     // recalculating
+    //     sum += partial_checksum(real_index / 2, sim_index,
+    // input[real_index]);
+    //
+    //     sim_indices[real_index] = sim_index;
+    //     sim_index += input[real_index];
+    //     sim_indices[real_index + 1] = sim_index;
+    //
+    //     let gap_size = input[real_index + 1];
+    //     heaps[gap_size].push(Reverse(sim_index));
+    //     sim_index += gap_size;
+    //
+    //     real_index += 2;
+    // }
 
-        sim_indices[real_index] = sim_index;
-        sim_index += input[real_index];
-        sim_indices[real_index + 1] = sim_index;
+    let mut idx = 0;
+    for (index, &size) in input.iter().enumerate() {
+        if index % 2 == 1 {
+            heaps[size].push(Reverse(idx));
+        }
 
-        let gap_size = input[real_index + 1];
-        heaps[gap_size].push(Reverse(sim_index));
-        sim_index += gap_size;
-
-        real_index += 2;
+        idx += size;
     }
 
     // Get the last element as well
-    sim_index += input[input.len() - 1];
-    sim_indices[input.len() - 1] = sim_index;
-    sum += partial_checksum(input.len() / 2, sim_index, input[input.len() - 1]);
+    // sim_index += input[input.len() - 1];
+    // sim_indices[input.len() - 1] = sim_index;
+    // sum += partial_checksum(input.len() / 2, sim_index, input[input.len() -
+    // 1]);
 
-    println!("Initial sum: {sum}");
+    // println!("Initial sum: {sum}");
 
     // println!("{heaps:?}");
 
-    let mut right = input.len() - 1;
-    while right > 0 {
-        let needed = input[right];
+    // while right > 0 {
+    //     let needed = input[right];
+    //
+    //     // Find a gap at least as large as the required size
+    //
+    //     'outer: for size in needed..10 {
+    //         // println!(
+    //         //     "Checking size {size}: {:?} | {}",
+    //         //     heaps[size].peek(),
+    //         //     sim_indices[right]
+    //         // );
+    //
+    //         match heaps[size].peek() {
+    //             Some(gap_index) if gap_index.0 < sim_indices[right] => {
+    //                 // Move the block
+    //                 let Some(gap_index) = heaps[size].pop() else {
+    //                     unreachable!()
+    //                 };
+    //
+    //                 // println!(
+    //                 //     "Move {needed} elements from {} to {}",
+    //                 //     sim_indices[right], gap_index.0
+    //                 // );
+    //
+    //                 sum += partial_checksum(right / 2, gap_index.0, needed);
+    //                 sum -=
+    //                     partial_checksum(right / 2, sim_indices[right],
+    // needed);
+    //
+    //                 // println!(
+    //                 //     "Adding: {}",
+    //                 //     partial_checksum(right / 2, gap_index.0, needed)
+    //                 // );
+    //                 // println!(
+    //                 //     "Removing: {}",
+    //                 //     partial_checksum(right / 2, sim_indices[right],
+    //                 // needed) );
+    //
+    //                 let rem = size - needed;
+    //                 let rem_index = gap_index.0 + needed;
+    //                 heaps[rem].push(Reverse(rem_index));
+    //
+    //                 // Stop searching
+    //                 break 'outer;
+    //             }
+    //             _ => (),
+    //         }
+    //     }
+    //
+    //     right -= 2;
+    // }
 
-        // Find a gap at least as large as the required size
-        for size in needed..10 {
-            // println!(
-            //     "Checking size {size}: {:?} | {}",
-            //     heaps[size].peek(),
-            //     sim_indices[right]
-            // );
+    let mut sum = 0;
 
-            match heaps[size].peek() {
-                Some(gap_index) if gap_index.0 < sim_indices[right] => {
-                    // Move the block
-                    let Some(gap_index) = heaps[size].pop() else {
+    for (index, &size) in input.iter().enumerate().rev() {
+        idx -= size;
+
+        if index % 2 == 1 {
+            continue;
+        }
+
+        let mut new_pos = idx;
+        let mut gap_size = usize::MAX;
+
+        for heap_size in size..10 {
+            match heaps[heap_size].peek() {
+                Some(gap_index) if gap_index.0 < new_pos => {
+                    let Some(Reverse(gap_index)) = heaps[heap_size].pop()
+                    else {
                         unreachable!()
                     };
 
-                    // println!(
-                    //     "Move {needed} elements from {} to {}",
-                    //     sim_indices[right], gap_index.0
-                    // );
-
-                    sum += partial_checksum(right / 2, gap_index.0, needed);
-                    sum -=
-                        partial_checksum(right / 2, sim_indices[right], needed);
-
-                    // println!(
-                    //     "Adding: {}",
-                    //     partial_checksum(right / 2, gap_index.0, needed)
-                    // );
-                    // println!(
-                    //     "Removing: {}",
-                    //     partial_checksum(right / 2, sim_indices[right],
-                    // needed) );
-
-                    let rem = size - needed;
-                    let rem_index = gap_index.0 + needed;
-                    heaps[rem].push(Reverse(rem_index));
-
-                    // Stop searching
-                    break;
+                    if gap_index < new_pos {
+                        new_pos = gap_index;
+                        gap_size = heap_size;
+                    }
                 }
                 _ => (),
             }
         }
 
-        right -= 2;
+        println!("Move {size} elements from {idx} to {new_pos}");
+
+        if gap_size != usize::MAX {
+            let remaining_gap = gap_size - size;
+            heaps[remaining_gap].push(Reverse(new_pos + size));
+
+            println!(
+                "Creating new gap of size {remaining_gap} at {}",
+                new_pos + size
+            );
+        }
+
+        sum += partial_checksum(index / 2, new_pos, size);
     }
 
     // println!("Heaps: {heaps:?}");
