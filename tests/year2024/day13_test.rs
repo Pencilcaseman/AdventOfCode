5082544b8cebcb8bf77c40e9a3d9a1a7972242fa
Toby Davis <pencilcaseman@gmail.com>
2025-09-15 16:44:51 +0100

    perf(year2024::day13): Remove edge case handling
    Seems like the edge case is never hit, so we can just ignore it. There's
    an note in the explanation

--- tests/year2024/day13_test.rs
+++ tests/year2024/day13_test.rs
@@ -17,42 +17,10 @@ Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279";
 
-const EDGE_CASE_1: &str = "\
-Button A: X+1, Y+2
-Button B: X+10, Y+20
-Prize: X=105, Y=210";
-
-const EDGE_CASE_2: &str = "\
-Button A: X+10, Y+20
-Button B: X+1, Y+2
-Prize: X=105, Y=210";
-
-const EDGE_CASE_3: &str = "\
-Button A: X+100, Y+200
-Button B: X+1, Y+2
-Prize: X=10, Y=20";
-
-const EDGE_CASE_4: &str = "\
-Button A: X+100, Y+200
-Button B: X+1, Y+2
-Prize: X=11, Y=20";
-
 #[test]
 fn test_part1() {
     let parsed = parse(EXAMPLE_1);
     assert_eq!(part1(&parsed), 480);
-
-    let parsed = parse(EDGE_CASE_1);
-    assert_eq!(part1(&parsed), 25);
-
-    let parsed = parse(EDGE_CASE_2);
-    assert_eq!(part1(&parsed), 35);
-
-    let parsed = parse(EDGE_CASE_3);
-    assert_eq!(part1(&parsed), 10);
-
-    let parsed = parse(EDGE_CASE_4);
-    assert_eq!(part1(&parsed), 0);
 }
 
 #[test]
