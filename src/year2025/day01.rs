a0c05ca1a4144361489f75a3ff9fbabd8effb99b
Toby Davis <toby@tobydavis.dev>
2025-12-03 00:53:37 +0000

    perf: Fast solution to `year2025::day01`
    - Combine solvers
    - Replace string iterators with pure byte-based looping
    - Remove a bit of duplicate computation

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -1,77 +1,72 @@
 use num_traits::Euclid;
 
-type Input = Vec<i32>;
+type Input = (i32, i32);
 
 pub fn parse(input: &str) -> Input {
-    input
-        .lines()
-        .map(|substr| {
-            let mut bytes = substr.bytes();
+    let mut p1: i32 = 0;
+    let mut p2: i32 = 0;
 
-            let sign: i32 = match bytes.next() {
-                Some(b'L') => -1,
-                Some(b'R') => 1,
-                _ => unreachable!(),
-            };
+    let mut angle = 50;
 
-            let num =
-                bytes.fold(0, |sum, byte| sum * 10 + (byte - b'0') as i32);
+    let mut dir = b'0';
+    let mut num = 0;
 
-            num * sign
-        })
-        .collect()
+    for byte in input.bytes() {
+        match byte {
+            b'L' => dir = b'L',
+            b'R' => dir = b'R',
+            b'\n' => {
+                let (full, rem) = num.div_rem_euclid(&100);
+
+                p2 += full;
+
+                if dir == b'R' {
+                    angle += rem;
+
+                    if angle > 99 {
+                        angle -= 100;
+                        p2 += 1;
+                    }
+                } else if dir == b'L' {
+                    let mut new_angle = angle - rem;
+
+                    if new_angle < 0 {
+                        new_angle += 100;
+
+                        // Passed zero
+                        if angle > 0 {
+                            p2 += 1;
+                        }
+                    } else if new_angle == 0 {
+                        // Landed on zero
+                        p2 += 1;
+                    }
+
+                    angle = new_angle;
+                }
+
+                if angle == 0 {
+                    p1 += 1;
+                }
+
+                dir = b'0';
+                num = 0;
+            }
+            digit => num = num * 10 + (digit - b'0') as i32,
+        }
+    }
+
+    (p1, p2)
 }
 
 pub fn part1(input: &Input) -> i32 {
-    input
-        .iter()
-        .fold((0, 50), |(mut count, mut angle), delta| {
-            angle = (angle + delta) % 100;
-            if angle == 0 {
-                count += 1;
-            }
-
-            (count, angle)
-        })
-        .0
+    input.0
 }
 
 pub fn part2(input: &Input) -> i32 {
-    input
-        .iter()
-        .fold((50, 0), |(mut angle, mut count), &delta| {
-            if delta > 0 {
-                let (full, rem) = delta.div_rem_euclid(&100);
-                count += full;
-
-                angle += rem;
-
-                if angle > 99 {
-                    angle -= 100;
-                    count += 1;
-                }
-            } else if delta < 0 {
-                let (full, rem) = (-delta).div_rem_euclid(&100);
-                count += full;
-
-                let mut new_angle = angle - rem;
-
-                if new_angle < 0 {
-                    new_angle += 100;
-
-                    if angle > 0 {
-                        count += 1;
-                    }
-                } else if new_angle == 0 {
-                    count += 1;
-                }
-
-                angle = new_angle;
-            }
-
-            (angle, count)
-        })
-        .1
+    input.1
 }
 
-// 6430 too high
+// Answers for my input
+// Part 1: 1078
+// Part 2: 6412
