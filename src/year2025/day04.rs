1e6b6fb2646fc1fe1edbfecaf5b48a7ee670c956
Toby Davis <toby@tobydavis.dev>
2025-12-06 00:25:35 +0000

    solve: year2025:day04

--- src/year2025/day04.rs
+++ src/year2025/day04.rs
@@ -1,17 +1,158 @@
-type Input = (u64, u64);
+use ndarray::Array2;
+use num_traits::WrappingAdd;
+
+use crate::util::{parse::grid_to_ndarray, point::Point2D};
+
+type Input = Array2<u8>;
 
 pub fn parse(input: &str) -> Input {
-    (0, 0)
+    grid_to_ndarray(input.bytes())
 }
 
-pub fn part1(input: &Input) -> u64 {
-    input.0
+pub fn part1(input: &Input) -> usize {
+    let offsets = [
+        Point2D::new(-1, -1), // up left
+        Point2D::new(-1, 0),  // up
+        Point2D::new(-1, 1),  // up right
+        //
+        Point2D::new(0, -1), // left
+        Point2D::new(0, 1),  // right
+        //
+        Point2D::new(1, -1), // down left
+        Point2D::new(1, 0),  // down
+        Point2D::new(1, 1),  // down right
+    ];
+
+    let mut grid = Array2::<u8>::zeros(input.dim());
+
+    let f = |(row, col): (i64, i64)| {
+        offsets
+            .iter()
+            .map(|offset| {
+                let pos = Point2D::new(row, col).wrapping_add(offset);
+
+                if pos.row < 0
+                    || pos.col < 0
+                    || pos.row >= input.dim().0 as i64
+                    || pos.col >= input.dim().1 as i64
+                {
+                    0
+                } else {
+                    match input[(pos.row as usize, pos.col as usize)] {
+                        b'@' => 1,
+                        _ => 0,
+                    }
+                }
+            })
+            .sum::<u8>()
+    };
+
+    grid.indexed_iter_mut().for_each(|((row, col), val)| {
+        if input[(row, col)] == b'@' {
+            *val = f((row as i64, col as i64))
+        } else {
+            *val = u8::MAX;
+        }
+    });
+
+    grid.iter().filter(|&&v| v < 4).count()
 }
 
 pub fn part2(input: &Input) -> u64 {
-    input.1
+    let offsets = [
+        Point2D::new(-1, -1), // up left
+        Point2D::new(-1, 0),  // up
+        Point2D::new(-1, 1),  // up right
+        //
+        Point2D::new(0, -1), // left
+        Point2D::new(0, 1),  // right
+        //
+        Point2D::new(1, -1), // down left
+        Point2D::new(1, 0),  // down
+        Point2D::new(1, 1),  // down right
+    ];
+
+    let mut grid = Array2::<u8>::zeros(input.dim());
+
+    let f = |(row, col): (i64, i64)| {
+        offsets
+            .iter()
+            .map(|offset| {
+                let pos = Point2D::new(row, col).wrapping_add(offset);
+
+                if pos.row < 0
+                    || pos.col < 0
+                    || pos.row >= input.dim().0 as i64
+                    || pos.col >= input.dim().1 as i64
+                {
+                    0
+                } else {
+                    match input[(pos.row as usize, pos.col as usize)] {
+                        b'@' => 1,
+                        _ => 0,
+                    }
+                }
+            })
+            .sum::<u8>()
+    };
+
+    grid.indexed_iter_mut().for_each(|((row, col), val)| {
+        if input[(row, col)] == b'@' {
+            *val = f((row as i64, col as i64))
+        } else {
+            *val = u8::MAX;
+        }
+    });
+
+    // Grid now contains the initial counts
+
+    let mut total_removed = 0;
+    loop {
+        let mut step_removed = 0;
+
+        let rows = grid.dim().0;
+        let cols = grid.dim().1;
+
+        for pos in ndarray::indices(grid.dim()) {
+            if grid[pos] < 4 {
+                // Paper can be removed, so decrement surrounding paper
+                grid[pos] = u8::MAX;
+                step_removed += 1;
+
+                offsets
+                    .iter()
+                    .filter_map(|o| {
+                        let new = o.wrapping_add(&Point2D::new(
+                            pos.0 as i64,
+                            pos.1 as i64,
+                        ));
+
+                        if new.row < 0
+                            || new.col < 0
+                            || new.row >= rows as i64
+                            || new.col >= cols as i64
+                        {
+                            None
+                        } else {
+                            Some(new)
+                        }
+                    })
+                    .for_each(|p| {
+                        grid[(p.row as usize, p.col as usize)] -= 1;
+                    })
+            }
+        }
+
+        total_removed += step_removed;
+
+        if step_removed == 0 {
+            break;
+        }
+    }
+
+    total_removed
 }
 
 // Answers for my input:
-// Part 1:
-// Part 2:
+// Part 1: 1553
+// Part 2: 8442
--- tests/year2025/day04_test.rs
+++ tests/year2025/day04_test.rs
@@ -1,15 +1,21 @@
 use aoc::year2025::day04::*;
 
 const EXAMPLE_INPUT: &str = "\
-987654321111111
-811111111111119
-234234234234278
-818181911112111";
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.";
 
 #[test]
 fn test_part1() {
     let input = parse(EXAMPLE_INPUT);
-    assert_eq!(part1(&input), 0);
+    assert_eq!(part1(&input), 13);
 }
 
 #[test]
