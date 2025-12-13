16126a22f8a59e2f55a6d58a4850ff6016f5df1b
Toby Davis <toby@tobydavis.dev>
2025-12-13 01:06:26 +0000

    Add Op enum

--- src/year2025/day06.rs
+++ src/year2025/day06.rs
@@ -1,6 +1,11 @@
 use crate::util::parse::ParseUnsigned;
 
-type Input<'a> = (Vec<&'a str>, &'a str);
+pub enum Op {
+    Sum,
+    Prod,
+}
+
+type Input<'a> = (Vec<&'a str>, Vec<Op>);
 
 pub fn parse(input: &'_ str) -> Input<'_> {
     let num_lines = input.bytes().filter(|&b| b == b'\n').count() + 1;
@@ -8,7 +13,16 @@ pub fn parse(input: &'_ str) -> Input<'_> {
     let mut lines = input.splitn(num_lines, '\n');
 
     let nums: Vec<&str> = lines.by_ref().take(num_lines - 1).collect();
-    let ops = lines.next().unwrap();
+    let ops = lines
+        .next()
+        .unwrap()
+        .bytes()
+        .filter_map(|b| match b {
+            b'+' => Some(Op::Sum),
+            b'*' => Some(Op::Prod),
+            _ => None,
+        })
+        .collect();
 
     (nums, ops)
 }
@@ -17,13 +31,14 @@ pub fn part1((nums, ops): &Input) -> u64 {
     let mut num_iterators: Vec<_> =
         nums.iter().map(|n| ParseUnsigned::<u64>::new(n.bytes())).collect();
 
-    ops.bytes()
-        .filter(|&b| b == b'+' || b == b'*')
+    ops.iter()
         .map(|op| {
-            let thing =
-                num_iterators.iter_mut().map(|iter| iter.next().unwrap());
+            let tmp = num_iterators.iter_mut().map(|iter| iter.next().unwrap());
 
-            if op == b'+' { thing.sum::<u64>() } else { thing.product() }
+            match op {
+                Op::Sum => tmp.sum::<u64>(),
+                Op::Prod => tmp.product(),
+            }
         })
         .sum()
 }
@@ -40,15 +55,11 @@ pub fn part2((nums, ops): &Input) -> u64 {
         }
     }
 
-    ops.bytes()
-        .filter(|&b| b == b'+' || b == b'*')
+    ops.iter()
         .zip(vertical_nums.split(|&n| n == 0))
-        .map(|(op, vert)| {
-            if op == b'+' {
-                vert.iter().sum::<u64>()
-            } else {
-                vert.iter().product()
-            }
+        .map(|(op, vert)| match op {
+            Op::Sum => vert.iter().sum::<u64>(),
+            Op::Prod => vert.iter().product(),
         })
         .sum()
 }
