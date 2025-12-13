1ec0bb095941bf45596c0ef697404caadaefea0f
Toby Davis <toby@tobydavis.dev>
2025-12-13 18:57:43 +0000

    perf: year2025::day01
    Slight performance improvement based on the simplified code developed in
    the previous commit.

--- src/year2025/day01.rs
+++ src/year2025/day01.rs
@@ -3,13 +3,12 @@
 //! Part 1 is quite simple -- just keep track of the angle of the dial
 //! (modulo 100) and count each time it lands on zero.
 //!
-//! Part 2 is a little more tricky, mainly due to left rotations.
+//! Part 2 is a little more tricky, mainly due to left rotations. To simplify
+//! things, we can treat left rotations as reversed right rotations.
 //!
 //! To improve performance slightly, we can "stream" the input bytes and
 //! "generate" each value as bytes come in, processing them when a new-line is
 //! seen (hence the need for the extra `std::iter::once(b'\n')`.)
-
-use crate::util::parse::ParseSigned;
 
 type Input = (i32, i32);
 
@@ -19,20 +18,31 @@ pub fn parse(input: &str) -> Input {
 
     let mut angle = 50;
 
-    let dirs = input.bytes().filter(u8::is_ascii_alphabetic);
-    let nums = ParseSigned::<i32>::new(input.bytes());
+    let mut dir = b'0';
+    let mut num = 0;
 
-    for (dir, num) in dirs.zip(nums) {
-        if dir == b'R' {
-            p2 += (angle + num) / 100;
-            angle = (angle + num) % 100;
-        } else {
-            // Treat left rotation as reversed right rotation
-            let reversed = (100 - angle) % 100;
-            p2 += (reversed + num) / 100;
-            angle = (angle - num).rem_euclid(100);
+    for byte in input.bytes().chain(std::iter::once(b'\n')) {
+        match byte {
+            b'L' => dir = b'L',
+            b'R' => dir = b'R',
+            b'\n' => {
+                if dir == b'R' {
+                    p2 += (angle + num) / 100;
+                    angle = (angle + num) % 100;
+                } else {
+                    // Treat left rotation as reversed right rotation
+                    let reversed = (100 - angle) % 100;
+                    p2 += (reversed + num) / 100;
+                    angle = (angle - num).rem_euclid(100);
+                }
+
+                p1 += (angle == 0) as i32;
+
+                dir = b'0';
+                num = 0;
+            }
+            digit => num = num * 10 + (digit - b'0') as i32,
         }
-        p1 += (angle == 0) as i32;
     }
 
     (p1, p2)
