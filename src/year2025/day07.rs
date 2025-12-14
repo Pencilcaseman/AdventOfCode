eb0faa5ca39c812747d24efa2d9a6fdeba9aa0b0
Toby Davis <toby@tobydavis.dev>
2025-12-14 14:27:36 -0800

    solve: year2025::day07::part2
    Almost exactly the same algorithm, except keeping track of the total
    number of ways of reaching a given cell at each step. The answer is then
    the sum of the total number of ways of reaching any end cell.

--- src/year2025/day07.rs
+++ src/year2025/day07.rs
@@ -35,10 +35,31 @@ pub fn part1(input: &Input) -> i32 {
     total
 }
 
-pub fn part2(input: &Input) -> i32 {
-    0
+pub fn part2(input: &Input) -> u64 {
+    let mut current = vec![0u64; input.dim().1];
+
+    for row in input.rows() {
+        for i in 0..input.dim().1 {
+            match row[i] {
+                b'.' => (),
+                b'S' => {
+                    current[i] = 1;
+                }
+                b'^' => {
+                    if current[i] > 0 {
+                        current[i - 1] += current[i];
+                        current[i + 1] += current[i];
+                        current[i] = 0;
+                    }
+                }
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    current.into_iter().sum()
 }
 
 // Answers for my input:
 // Part 1: 1662
-// Part 2:
+// Part 2: 40941112789504
