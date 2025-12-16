c3948557162ee5a6a6e96f6a15717d2c42c862fe
Toby Davis <toby@tobydavis.dev>
2025-12-15 20:21:52 -0800

    perf: year2025::day08
    Parallelising the input loading and chunking edges into buckets based on
    their length gives a huge performance improvement. Lazily sorting those
    buckets when required also makes things a lot faster.
    
    There may be some more room to improve the number of buckets and the
    range of values that go into each bucket, but the room for improvement
    is likely quite small.

--- src/year2025/day08.rs
+++ src/year2025/day08.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 use itertools::Itertools;
 use rayon::prelude::*;
 
@@ -7,7 +5,10 @@ use crate::util::parse::ParseUnsigned;
 
 type Input = (u32, u32);
 
-const CUTOFF_LIMIT: usize = 25;
+type BucketThreadEdge = Vec<Vec<Vec<(u64, u16, u16)>>>;
+
+const NUM_BUCKETS: usize = 8;
+const DIST_MAGNITUDE: u64 = 15_000 * 15_000;
 
 pub fn parse(input: &str) -> Input {
     solve(ParseUnsigned::<u32>::new(input.bytes()).tuples().collect(), 1000)
@@ -29,108 +30,106 @@ fn dist(a: &(u32, u32, u32), b: &(u32, u32, u32)) -> u64 {
     dx * dx + dy * dy + dz * dz
 }
 
-pub fn solve(input: Vec<(u32, u32, u32)>, steps: usize) -> (u32, u32) {
-    let mut pairwise_distances = Vec::new();
-    let mut cutoff = u64::MAX;
+pub fn parallel_load(input: &[(u32, u32, u32)]) -> BucketThreadEdge {
+    let mut buckets = vec![vec![]; NUM_BUCKETS];
+    let chunk_size = input.len() / rayon::current_num_threads();
+    let indices: Vec<_> = (0..input.len()).collect();
 
-    for i in 0..input.len() {
-        for j in (i + 1)..input.len() {
-            let d = dist(&input[i], &input[j]);
-            if d < cutoff {
-                pairwise_distances.push((d, i as u32, j as u32));
+    for b in indices
+        .par_chunks(chunk_size)
+        .map(|is| {
+            let mut buckets = vec![vec![]; NUM_BUCKETS];
+
+            for &i in is {
+                for j in i + 1..input.len() {
+                    let d = dist(&input[i], &input[j]);
+
+                    let bucket =
+                        (d / DIST_MAGNITUDE).min(NUM_BUCKETS as u64 - 1);
+                    buckets[bucket as usize].push((d, i as u16, j as u16));
+                }
             }
-        }
 
-        if i == CUTOFF_LIMIT {
-            pairwise_distances.par_sort_unstable_by_key(|x| x.0);
-            cutoff = pairwise_distances[steps - 1].0;
-        }
+            buckets
+        })
+        .collect::<Vec<_>>()
+    {
+        buckets.iter_mut().zip(b).for_each(|(dst, b)| dst.push(b));
     }
 
-    pairwise_distances.par_sort_unstable_by_key(|x| x.0);
+    buckets
+}
 
-    let mut iter = pairwise_distances.into_iter();
-    let mut dsu = DisjointSetUnion::new(input.len());
+pub fn solve(input: Vec<(u32, u32, u32)>, steps: usize) -> (u32, u32) {
+    let buckets = parallel_load(&input);
+    let mut iter = flatten(&buckets);
+
+    let mut dsu_parents: Vec<_> = (0..input.len()).collect();
+    let mut dsu_counts = vec![1; input.len()];
 
     for (_, i, j) in iter.by_ref().take(steps) {
         let (i, j) = (i as usize, j as usize);
-        dsu.merge(i, j);
+        dsu_merge(&mut dsu_parents, &mut dsu_counts, i, j);
     }
 
-    let mut counts = dsu.counts().to_vec();
+    let mut counts = dsu_counts.clone();
     counts.sort_unstable();
-    let part1 = counts.into_iter().rev().take(3).product();
+    let part1: u32 = counts.into_iter().rev().take(3).product();
+
     let mut part2 = 0;
 
     for (_, i, j) in iter {
         let (i, j) = (i as usize, j as usize);
-        if dsu.merge(i, j) == input.len() as u32 {
+        if dsu_merge(&mut dsu_parents, &mut dsu_counts, i, j)
+            == input.len() as u32
+        {
             part2 = input[i].0 * input[j].0;
+            break;
         }
     }
 
     (part1, part2)
 }
 
-#[derive(Debug)]
-struct DisjointSetUnion {
-    parents: Vec<usize>,
-    ranks: Vec<u8>,
-    counts: Vec<u32>,
+fn flatten(
+    buckets: &BucketThreadEdge,
+) -> impl Iterator<Item = (u64, u16, u16)> {
+    buckets.iter().flat_map(|b| {
+        let mut merged = b.concat();
+        merged.sort_unstable_by_key(|v| v.0);
+        merged
+    })
 }
 
-impl DisjointSetUnion {
-    fn new(n: usize) -> Self {
-        Self {
-            parents: (0..n).collect(),
-            ranks: vec![1; n],
-            counts: vec![1; n],
-        }
+fn dsu_find(parents: &mut [usize], mut i: usize) -> usize {
+    while parents[i] != i {
+        let parent = parents[i];
+        parents[i] = parents[parent];
+        i = parent;
     }
 
-    fn parent(&mut self, mut i: usize) -> usize {
-        while i != self.parents[i] {
-            let p = self.parents[i];
-            self.parents[i] = self.parents[p];
-            i = self.parents[i];
+    i
+}
+
+fn dsu_merge(
+    parents: &mut [usize],
+    counts: &mut [u32],
+    i: usize,
+    j: usize,
+) -> u32 {
+    let mut parent_i = dsu_find(parents, i);
+    let mut parent_j = dsu_find(parents, j);
+
+    if parent_i != parent_j {
+        if counts[parent_i] > counts[parent_j] {
+            (parent_i, parent_j) = (parent_j, parent_i);
         }
-        i
+
+        counts[parent_j] += counts[parent_i];
+        parents[parent_i] = parent_j;
     }
 
-    fn merge(&mut self, i: usize, j: usize) -> u32 {
-        let parent_i = self.parent(i);
-        let parent_j = self.parent(j);
-
-        if parent_i == parent_j {
-            // Already in the same circuit
-            return 0;
-        }
-
-        // Join the smaller tree onto the larger one
-        match self.ranks[parent_i].cmp(&self.ranks[parent_j]) {
-            Ordering::Less => {
-                self.counts[parent_j] += self.counts[parent_i];
-                self.parents[parent_i] = parent_j;
-                self.counts[parent_j]
-            }
-            Ordering::Greater => {
-                self.counts[parent_i] += self.counts[parent_j];
-                self.parents[parent_j] = parent_i;
-                self.counts[parent_i]
-            }
-            Ordering::Equal => {
-                // Tree grows by 1
-                self.counts[parent_i] += self.counts[parent_j];
-                self.parents[parent_j] = parent_i;
-                self.ranks[parent_i] += 1;
-                self.counts[parent_i]
-            }
-        }
-    }
-
-    fn counts(&self) -> &[u32] {
-        &self.counts
-    }
+    counts[parent_j]
 }
 
 // Answers for my input:
