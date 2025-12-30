7a6d11bd35c82c7519f9e8a05f66a698df19025b
Toby Davis <toby@tobydavis.dev>
2025-12-30 19:39:55 +0000

    solve: year2025::day11::part2
    By only considering nodes connected to the target, we can remove a lot
    of paths that cannot possibly be valid.
    
    Additionally, DAC and FFT act as bottlenecks for the valid paths, so we
    can compute (svr -> fft) * (fft -> dac) * (dac -> out) to massively
    reduce the amount of paths that need to be considered.

--- src/year2025/day11.rs
+++ src/year2025/day11.rs
@@ -1,20 +1,49 @@
 use std::collections::{HashMap, HashSet, VecDeque, hash_map::Entry};
 
-type Input<'a> = HashMap<&'a str, Vec<&'a str>>;
+use rustc_hash::FxBuildHasher;
+
+type FastHashMap<K, V> = HashMap<K, V, FxBuildHasher>;
+type FastHashSet<K> = HashSet<K, FxBuildHasher>;
+
+#[derive(Default, Debug)]
+pub struct Node<'a> {
+    forward: Vec<&'a str>,
+    backward: Vec<&'a str>,
+}
+
+type Input<'a> = FastHashMap<&'a str, Node<'a>>;
 
 pub fn parse<'a>(input: &'a str) -> Input<'a> {
+    // TODO: Store graph as a matrix: row -> col ==> Node[row] -> Node[col]
+
     let mut graph = Input::default();
 
     for line in input.lines() {
-        let (from, to) = line.split_at(3);
+        let (from, to_many) = line.split_at(3);
 
-        match graph.entry(from) {
-            Entry::Occupied(mut occupied_entry) => occupied_entry
-                .get_mut()
-                .extend(to[2..].split_ascii_whitespace()),
-            Entry::Vacant(vacant_entry) => {
-                vacant_entry
-                    .insert_entry(to[2..].split_ascii_whitespace().collect());
+        for to in to_many[2..].split_ascii_whitespace() {
+            match graph.entry(from) {
+                Entry::Occupied(mut occupied_entry) => {
+                    occupied_entry.get_mut().forward.push(to)
+                }
+                Entry::Vacant(vacant_entry) => {
+                    vacant_entry.insert_entry(Node {
+                        forward: vec![to],
+                        backward: Vec::new(),
+                    });
+                }
+            }
+
+            match graph.entry(to) {
+                Entry::Occupied(mut occupied_entry) => {
+                    occupied_entry.get_mut().backward.push(from)
+                }
+                Entry::Vacant(vacant_entry) => {
+                    vacant_entry.insert_entry(Node {
+                        forward: Vec::new(),
+                        backward: vec![from],
+                    });
+                }
             }
         }
     }
@@ -22,16 +51,34 @@ pub fn parse<'a>(input: &'a str) -> Input<'a> {
     graph
 }
 
-pub fn part1(input: &Input) -> u32 {
-    let mut stack = vec!["you"];
+pub fn part1(input: &Input) -> u64 {
+    solve(input, "you", "out", None)
+}
+
+pub fn part2(input: &Input) -> u64 {
+    solve(input, "svr", "fft", Some(&connected_to(input, "fft")))
+        * solve(input, "fft", "dac", Some(&connected_to(input, "dac")))
+        * solve(input, "dac", "out", Some(&connected_to(input, "out")))
+}
+
+pub fn solve(
+    input: &Input,
+    src: &str,
+    dst: &str,
+    connected: Option<&FastHashSet<&str>>,
+) -> u64 {
+    let mut stack = vec![src];
     let mut res = 0;
 
     while let Some(current) = stack.pop() {
-        for next in &input[current] {
-            if *next != "out" {
-                stack.push(next);
-            } else {
+        for &next in &input[current].forward {
+            if next == dst {
                 res += 1;
+                break;
+            }
+
+            if connected.is_none_or(|c| c.contains(next)) {
+                stack.push(next);
             }
         }
     }
@@ -39,28 +86,27 @@ pub fn part1(input: &Input) -> u32 {
     res
 }
 
-pub fn part2(input: &Input) -> u64 {
-    let mut stack = vec![("svr", false, false)];
-    let mut res = 0;
+fn connected_to<'a>(graph: &'a Input, node: &'a str) -> FastHashSet<&'a str> {
+    let mut connected = FastHashSet::<&'a str>::default();
 
-    while let Some((current, mut seen_dac, mut seen_fft)) = stack.pop() {
-        for &next in &input[current] {
-            if next == "out" {
-                res += (seen_dac && seen_fft) as u64;
-                break;
-            } else if next == "dac" {
-                seen_dac = true;
-            } else if next == "fft" {
-                seen_fft = true;
+    connected.insert(node);
+
+    let mut stack = vec![node];
+
+    while let Some(next) = stack.pop() {
+        // Backward connections are all connected.
+        // Forward connections from those backward connections may not be.
+
+        for backward in &graph[next].backward {
+            if connected.insert(backward) {
+                stack.push(backward);
             }
-
-            stack.push((next, seen_dac, seen_fft));
         }
     }
 
-    res
+    connected
 }
 
 // Answers for my input:
 // Part 1: 494
-// Part 2:
+// Part 2: 296006754704850
