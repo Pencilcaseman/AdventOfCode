cde26dd72e9946f7d3ceeed9d2d335a5a2a8c181
Toby Davis <toby@tobydavis.dev>
2026-01-09 19:10:17 +0000

    wip

--- src/year2025/day12.rs
+++ src/year2025/day12.rs
@@ -1,90 +1,57 @@
-// All presents are 3x3
-// 6 present shapes?
-
 use crate::util::parse::ParseUnsigned;
 
-type Present = u8;
+const PRESENT_SIZE: u32 = 9;
+const NUM_PRESENTS: usize = 8;
 
 #[derive(Debug)]
 pub struct Tree {
-    rows: u8,
-    cols: u8,
-    counts: Vec<u8>,
+    rows: u32,
+    cols: u32,
+    counts: [u32; NUM_PRESENTS],
 }
 
-type Input = (Vec<Present>, Vec<Tree>);
+type Input = Vec<Tree>;
 
 pub fn parse(input: &str) -> Input {
-    let mut presents = Vec::new();
     let mut trees = Vec::new();
 
-    // TODO: More efficient parsing algorithm?
-
-    let mut parsing_presents = true;
-    let mut current = 0;
-
     for line in input.lines() {
-        // Check if this line is not a present
-        if line.contains('x') {
-            parsing_presents = false;
+        let bytes = line.as_bytes();
+
+        if bytes.len() < 3 || bytes[2] != b'x' {
+            continue;
         }
 
-        if parsing_presents {
-            if line.is_empty() {
-                presents.push(current);
-                current = 0;
-            } else {
-                current += line.bytes().filter(|&b| b == b'#').count() as u8;
-            }
-        } else {
-            let mut nums = ParseUnsigned::<u8>::new(line.bytes());
+        let mut nums = ParseUnsigned::<u8>::new(line.bytes()).map(|x| x as u32);
 
-            let rows = nums.next().unwrap();
-            let cols = nums.next().unwrap();
-            let counts = nums.collect();
+        let rows = nums.next().unwrap();
+        let cols = nums.next().unwrap();
 
-            trees.push(Tree { rows, cols, counts });
-        }
+        let mut counts = [0; NUM_PRESENTS];
+        nums.enumerate().for_each(|(i, n)| counts[i] = n);
+
+        trees.push(Tree { rows, cols, counts });
     }
 
-    (presents, trees)
+    trees
 }
 
-pub fn part1((presents, trees): &Input) -> u32 {
-    let mut impossible = 0;
+pub fn part1(input: &Input) -> u32 {
     let mut trivial = 0;
 
-    for tree in trees {
-        // Presents cannot possibly fit under the tree
-        let min_area: u32 = tree
-            .counts
-            .iter()
-            .zip(presents)
-            .map(|(&count, &area)| (count as u32) * (area as u32))
-            .sum();
-
-        let tree_area = tree.rows as u32 * tree.cols as u32;
-        if min_area > tree_area {
-            impossible += 1;
-        }
-
+    for tree in input {
         // Presents fit trivially under the tree
-        let rounded_rows = (tree.rows as u32 / 3) * 3;
-        let rounded_cols = (tree.cols as u32 / 3) * 3;
+        let rounded_rows = (tree.rows / 3) * 3;
+        let rounded_cols = (tree.cols / 3) * 3;
         let rounded_area = rounded_rows * rounded_cols;
 
         // All presents are 3x3
-        let max_area =
-            9 * tree.counts.iter().copied().map(u32::from).sum::<u32>();
+        let max_area = PRESENT_SIZE * tree.counts.iter().sum::<u32>();
 
         if max_area <= rounded_area {
             trivial += 1;
         }
     }
-
-    println!("Total: {}", trees.len());
-    println!("Impossible: {impossible}");
-    println!("Trivial: {trivial}");
 
     trivial
 }
@@ -94,5 +61,5 @@ pub fn part2(input: &Input) -> u32 {
 }
 
 // Answers for my input:
-// Part 1:
-// Part 2:
+// Part 1: 469
+// Part 2: No answer required :D
