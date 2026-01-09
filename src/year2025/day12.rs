2ebe16cbc350d61551bb5354a1ed73ffef78906c
Toby Davis <toby@tobydavis.dev>
2026-01-09 18:28:09 +0000

    setup: year2025::day12

--- src/year2025/day12.rs
+++ src/year2025/day12.rs
@@ -1,15 +1,26 @@
-type Input = (u32, u32);
+// All presents are 3x3
+// 6 present shapes?
+
+type Present = u8;
+
+struct Tree {
+    rows: u8,
+    cols: u8,
+    presents: Vec<u8>,
+}
+
+type Input = (Vec<Present>, Tree);
 
 pub fn parse(input: &str) -> Input {
-    (0, 0)
+    todo!()
 }
 
 pub fn part1(input: &Input) -> u32 {
-    input.0
+    0
 }
 
 pub fn part2(input: &Input) -> u32 {
-    input.1
+    0
 }
 
 // Answers for my input:
