cc09d833a41056e1ef2b30817834ca38b49be197
Toby Davis <toby@tobydavis.dev>
2026-01-14 16:12:59 +0000

    simd: year2025::day04::parse
    Replaced the old parsing method with a much more efficient SIMD
    implementation.

--- src/year2025/day04.rs
+++ src/year2025/day04.rs
@@ -1,16 +1,24 @@
+use std::simd::{
+    Simd, SimdCast,
+    cmp::{SimdPartialEq, SimdPartialOrd},
+    i8x64,
+    num::SimdInt,
+    u8x64,
+};
+
 use ndarray::Array2;
 
 type Input = (Vec<(usize, usize)>, Array2<u8>);
 
 const OFFSETS: [(usize, usize); 8] = [
     (usize::MAX, usize::MAX), // -1 -1
-    (usize::MAX, 0),          // -1 0
-    (usize::MAX, 1),          // -1 1
-    (0, usize::MAX),          // 0 -1
-    (0, 1),                   // 0 1
-    (1, usize::MAX),          // 1 -1
-    (1, 0),                   // 1 0
-    (1, 1),                   // 1 1
+    (usize::MAX, 0),          // -1  0
+    (usize::MAX, 1),          // -1  1
+    (0, usize::MAX),          //  0 -1
+    (0, 1),                   //  0  1
+    (1, usize::MAX),          //  1 -1
+    (1, 0),                   //  1  0
+    (1, 1),                   //  1  1
 ];
 
 fn process_row<const HAS_ABOVE: bool, const HAS_BELOW: bool>(
@@ -28,7 +36,11 @@ fn process_row<const HAS_ABOVE: bool, const HAS_BELOW: bool>(
     let row_offset = r * cols;
     let counts = &mut out_counts[row_offset..row_offset + cols];
 
-    // First column
+    // Starting at column 1 and ending at cols - 1
+    let end_simd = if cols > 64 + 2 { cols - 64 - 1 } else { 0 };
+    let at_splat = Simd::splat(b'@');
+
+    // First column (scalar)
     if curr[0] == b'@' {
         let mut sum = (curr[1] == b'@') as u8;
 
@@ -36,6 +48,7 @@ fn process_row<const HAS_ABOVE: bool, const HAS_BELOW: bool>(
             let a = above.unwrap();
             sum += (a[0] == b'@') as u8 + (a[1] == b'@') as u8;
         }
+
         if HAS_BELOW {
             let b = below.unwrap();
             sum += (b[0] == b'@') as u8 + (b[1] == b'@') as u8;
@@ -48,8 +61,63 @@ fn process_row<const HAS_ABOVE: bool, const HAS_BELOW: bool>(
         }
     }
 
-    // Middle columns
-    for c in 1..(cols - 1) {
+    let mut c = 1;
+    while c < end_simd {
+        let chunk = u8x64::from_slice(&curr[c..]);
+        let mask = chunk.simd_eq(at_splat);
+
+        let mut acc = i8x64::splat(0);
+
+        #[inline(always)]
+        fn acc_row(acc: &mut i8x64, slice: &[u8], idx: usize, target: u8x64) {
+            let l = Simd::from_slice(&slice[idx - 1..]);
+            let m = Simd::from_slice(&slice[idx..]);
+            let r = Simd::from_slice(&slice[idx + 1..]);
+
+            // acc -= (-1) is equivalent to acc += 1
+            *acc -= l.simd_eq(target).to_int();
+            *acc -= m.simd_eq(target).to_int();
+            *acc -= r.simd_eq(target).to_int();
+        }
+
+        let mid_l = Simd::from_slice(&curr[c - 1..]);
+        let mid_r = Simd::from_slice(&curr[c + 1..]);
+        acc -= mid_l.simd_eq(at_splat).to_int();
+        acc -= mid_r.simd_eq(at_splat).to_int();
+
+        if HAS_ABOVE {
+            unsafe {
+                acc_row(&mut acc, above.unwrap_unchecked(), c, at_splat);
+            }
+        }
+
+        // -- Bottom Row Neighbors --
+        if HAS_BELOW {
+            unsafe {
+                acc_row(&mut acc, below.unwrap_unchecked(), c, at_splat);
+            }
+        }
+
+        let sums = acc.cast::<u8>();
+
+        // Update counts where cell is '@'
+        let old_vals = u8x64::from_slice(&counts[c..]);
+        let new_vals = mask.select(sums, old_vals);
+        new_vals.copy_to_slice(&mut counts[c..c + 64]);
+
+        // Find todo values
+        let mut todo_mask = (mask & sums.simd_lt(Simd::splat(4))).to_bitmask();
+        while todo_mask != 0 {
+            let i = todo_mask.trailing_zeros();
+            todo.push((r, c + i as usize));
+            todo_mask ^= 1 << i;
+        }
+
+        c += 64;
+    }
+
+    // Handle remaining columns before the end
+    for c in c..(cols - 1) {
         if curr[c] != b'@' {
             continue;
         }
@@ -57,14 +125,14 @@ fn process_row<const HAS_ABOVE: bool, const HAS_BELOW: bool>(
         let mut sum = (curr[c - 1] == b'@') as u8 + (curr[c + 1] == b'@') as u8;
 
         if HAS_ABOVE {
-            let a = above.unwrap();
+            let a = unsafe { above.unwrap_unchecked() };
             sum += (a[c - 1] == b'@') as u8
                 + (a[c] == b'@') as u8
                 + (a[c + 1] == b'@') as u8;
         }
 
         if HAS_BELOW {
-            let b = below.unwrap();
+            let b = unsafe { below.unwrap_unchecked() };
             sum += (b[c - 1] == b'@') as u8
                 + (b[c] == b'@') as u8
                 + (b[c + 1] == b'@') as u8;
